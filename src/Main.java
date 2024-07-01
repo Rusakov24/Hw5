a30cd4549500eac08edaacc42f10f0155724ae58
Rusakov24 <lovewillkillme@vk.com>
2024-07-01 18:01:22 +0300

    Hw5

--- src/Main.java
+++ src/Main.java
@@ -60,6 +60,39 @@ public class Main {
         } else {
             System.out.println("Доставка на расстояние свыше 100 км не осуществляется");
         }
+        System.out.println("Задание 5");
+        int monthNumber = 12;
+        if (monthNumber < 1 || monthNumber > 12) {
+            System.out.println("Некорректный номер месяца. Введите число от 1 до 12.");
+        } else {
+            String season;
+            switch (monthNumber) {
+                case 12:
+                case 1:
+                case 2:
+                    season = "зима";
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    season = "весна";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = "лето";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = "осень";
+                    break;
+                default:
+                    season = "неизвестный сезон";
+                    break;
+            }
+            System.out.println(monthNumber + "-й месяц принадлежит к сезону " + season + ".");
+        }
     }
 }
 
